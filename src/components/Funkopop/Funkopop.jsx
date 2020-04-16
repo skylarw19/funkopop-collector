fcced3eca55ddd483edeb3234fc6cdc39600df7f
Skylar Wong <skylar@Skylars-MacBook-Air.local>
2020-04-16 15:22:25 -0500

    update CSS for buttons

--- src/components/Funkopop/Funkopop.jsx
+++ src/components/Funkopop/Funkopop.jsx
@@ -5,19 +5,25 @@ import {Link} from 'react-router-dom';
 const Funkopop = (props) => {
   {if(props.funkopop.user === props.user._id) 
     return (
-      <div className="funkopop">
-        <h3>Name: {props.funkopop.name}</h3>
-        <p>Category: {props.funkopop.category}</p>
-        <p>Item No: {props.funkopop.itemNo}</p>
-        <p>Exclusivity: {props.funkopop.exclusivity}</p>
-        <Link to={{
-            pathname: "/edit",
-            state: props.funkopop
-        }}>  
-        UPDATE</Link>
-        <button
-          onClick={()=> props.handleDeleteFunko(props.funkopop._id)}
-        >DELETE</button>
+      <div className="panel panel-default">
+        <h3 className="panel-heading panel-title">{props.funkopop.name}</h3>
+        <div className="panel-body">
+          <p>Category: {props.funkopop.category}</p>
+          <p>Item No: {props.funkopop.itemNo}</p>
+          <p>Exclusivity: {props.funkopop.exclusivity}</p>
+          
+          <Link 
+            className="btn btn-primary"
+            to={{
+              pathname: "/edit",
+              state: props.funkopop
+          }}>  
+          UPDATE</Link>
+          <button
+            className="btn btn-danger"
+            onClick={()=> props.handleDeleteFunko(props.funkopop._id)}
+          >DELETE</button>
+        </div>
       </div>
     )
   else return (<> </>)
